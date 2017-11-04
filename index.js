0d55c3c117aab015cdd6d277e74614b292271321
austinfrey <aafrey85@gmail.com>
2017-11-04 03:07:45 +0000

    All OF functions now triggered by Level key changes. State is always read from db `NextState` and data return from OF functions always persisted in `NextData`
    Signed-off-by: austinfrey <aafrey85@gmail.com>

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const ttl = require('level-ttl')
 const hooks = require('level-hooks')
 
 const stateJSON = require('./state.json')
-const {task, parallel} = require('./step-functions')
+const {parallel} = require('./step-functions')
 
 require('net').createServer().listen();
 
@@ -13,15 +13,20 @@ const db = ttl(level('./db', {valueEncoding: 'json'}), {checkFrequency: 50})
 
 hooks(db)
 
-db.hooks.post('!ttl!Wait', async(change) => {
-	if(change.type === 'del') {
-		try {
-			const state = await db.get('NextState')
-			const data = await db.get('NextData')
-			await triggerFunction(false)
-		} catch(err) {
-			console.log(err)
-		}
+db.hooks.post('!ttl!', async(change) => {
+	const val = change.key.toString('utf8')
+	const state = await db.get('NextState')
+	const data = await db.get('NextData')
+	const stateType = {
+		  '!ttl!Task': task
+		, '!ttl!Wait': wait
+	}
+	if(
+		change.type === 'del'
+		&& (val === '!ttl!Task' || val === '!ttl!Wait'))
+	{
+		console.log(state.Type, state.Resource || 'none')
+		return stateType[val](state, data, next)
 	}
 })
 
@@ -33,30 +38,39 @@ async function runStepFunction() {
 	const states = stateJSON.States
 	await db.put('NextState', states[startAt])
 	await db.put('NextData', '')
-	await triggerFunction()
+	await db.put('Task', states[startAt], {ttl: 75})
 }
 
-async function triggerFunction(isDone) {
-	const data = await db.get('NextData')
-	if(isDone) {
-		return console.log(data)
-	}
-	const state = await db.get('NextState')
-	const stateTypes = {'Task': task, 'Wait': wait, 'Parallel': parallel}
-	const type = state.Type
-
-	return await stateTypes[type](state, data, next)
+async function finished() {
+	console.log(await db.get('NextData'))
 }
 
 async function next(results) {
 	const state = await db.get('NextState')
-	await db.put('NextState', stateJSON.States[state.Next])
-	await db.put('NextData', results)
-	await triggerFunction(!!state['End'])
+
+	if(state.Next) {
+		stateJSON.States[state.Next].Type === 'Wait'
+			? await db.put('Wait', state, {ttl: stateJSON.States[state.Next].Seconds * 1000})
+			: await db.put(stateJSON.States[state.Next].Type, state, {ttl: 75})
+	}
+	if(state.End) {
+		await db.put('NextData', results)
+		return finished()
+	}
+	await db.put('NextState', stateJSON.States[state.Next] || '')
+	results ? await db.put('NextData', results) : () => {}
+}
+
+async function task(state, data, next) {
+	try {
+		const result = await openfaas.invoke(state["Resource"], data, isJson = false)
+		await next(result.body)
+	} catch(err) {
+		console.log(err)
+	}
 }
 
 async function wait(state, data, next) {
-	console.log(state.Type)
-	await db.put('Wait', 'foo' ,{ttl: state['Seconds'] * 1000})
-	await db.put('NextState', stateJSON.States[state.Next])
+	await next()
 }
+
